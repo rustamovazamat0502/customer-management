2c9fd72d6d8b6da7174eb2f931a6c669c12cc4cf
rustamovazamat0502 <104127738+rustamovazamat0502@users.noreply.github.com>
2022-08-24 12:51:42 +0500

    New Changes

--- account_project/settings.py
+++ account_project/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-EMAIL_BACKEND = "djando.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_TLS = True
