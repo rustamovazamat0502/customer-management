14e813a4062b2ba0cf1d887d7c9918cf8f8f6f62
rustamovazamat0502 <104127738+rustamovazamat0502@users.noreply.github.com>
2022-09-08 14:26:43 +0500

    database changed

--- account_project/settings.py
+++ account_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'account_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "customer_management",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
